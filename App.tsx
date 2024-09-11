08933acefc8fd91d9daf6af3357a33905ccbb597
rkm10 <rajkumarmalluri08@gmail.com>
2024-09-11 17:47:42 +0530

    new Team

--- App.tsx
+++ App.tsx
@@ -1,27 +1,30 @@
 import react, { useState } from 'react';
-import { Image, Text, TextInput, View } from 'react-native';
+import { Image, Text, TextInput, View, ScrollView } from 'react-native';
 
 export default function App() {
   const [text, setText] = useState('');
 
   return (
-    <View
-      style={{
-        // flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text style={{ fontSize: 30, paddingBottom: 20 }}>Hello, I am your cat.....!</Text>
-      <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png', width: 200, height: 200 }} />
-      <TextInput onChangeText={newText => setText(newText)} defaultValue={text} placeholder="Type here to cats!"
-        style={{ height: 40 }} />
-      <Text style={{ padding: 10, fontSize: 42 }}>
-        {text
-          .split('')
-          .map(word => word && '🐱')
-          .join('')
-        }
-      </Text>
-    </View>
+    <ScrollView>
+
+      <View
+        style={{
+          // flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{ fontSize: 30, paddingBottom: 20 }}>Hello, I am your cat.....!</Text>
+        <Image source={{ uri: 'https://reactnative.dev/docs/assets/p_cat2.png', width: 200, height: 200 }} />
+        <TextInput onChangeText={newText => setText(newText)} defaultValue={text} placeholder="Type here to cats!"
+          style={{ height: 40 }} />
+        <Text style={{ padding: 10, fontSize: 42 }}>
+          {text
+            .split('')
+            .map(word => word && '🐱')
+            .join('')
+          }
+        </Text>
+      </View>
+    </ScrollView>
   );
 }
